8b183986c5ac1b811ef726ec13acea9438aa761b
Amaury <amaury.data@gmail.com>
2025-01-05 00:42:14 +0800

    Update Siemens-YunXueTang-Assistant.user.js

--- Siemens-YunXueTang-Assistant.user.js
+++ Siemens-YunXueTang-Assistant.user.js
@@ -13,25 +13,23 @@
 
     // 定时任务，10秒循环
     setInterval(() => {
-        // 获取当前页面的URL
+        // 判断当前页是播放页还是列表页
         const currentUrl = window.location.href;
-
-        // 判断页面类型：是否是列表页面或者视频播放页面
-        const isListPage = currentUrl.includes('#/list');
         const isVideoPage = currentUrl.includes('#/video');
+        const isListPage = currentUrl.includes('#/list');
 
         if (isVideoPage) {
-            console.log("执行视频播放页子程序");
+            console.log("执行播放页子程序");
             handleVideoPage();
         } else if (isListPage) {
-            console.log("执行视频列表页子程序");
+            console.log("执行列表页子程序");
             handleListPage();
         } else {
             console.log("无法识别页面类型");
         }
-    }, 10000); // 每10秒运行一次
+    }, 10000);
 
-    // 视频播放页处理
+    // 播放页子程序
     function handleVideoPage() {
         // 自动静音
         const mediaElements = document.querySelectorAll('video, audio');
@@ -54,44 +52,43 @@
             const text = button.textContent.trim();
             if ((text === "开始学习" || text === "继续学习") && button.offsetWidth > 0) {
                 button.click();
-                console.log('自动点击了按钮：' + text);
+                console.log('自动点击' + text);
                 isPlaying = true;
             }
         });
         // 未播放自动返回上页
         if (!isPlaying) {
-            console.log("检测到停止播放，返回上一页...");
+            console.log("检测到未播放，返回上一页");
             window.history.back();
             setTimeout(() => {
-                window.location.reload(); // 延迟一段时间后刷新页面
-            }, 1000); // 延迟1秒后刷新，确保页面能够跳转
+                window.location.reload();
+            }, 1000);
         }
     }
 
-    // 列表页处理
+    // 列表页子程序
     function handleListPage() {
-        // 自动寻找未看过的视频
+        // 自动查找未学习的视频
         const items = Array.from(document.querySelectorAll('.kng-list-new__tags')).filter(tagDiv => {
             const tagTexts = Array.from(tagDiv.querySelectorAll('.kng-list-new__tag')).map(tag => tag.textContent.trim());
             return tagTexts.includes("视频") && !tagTexts.includes("已学完");
         }).map(tagDiv => tagDiv.closest('li'));
-
+        // 找到视频自动播放
         if (items.length > 0) {
             const randomItem = items[Math.floor(Math.random() * items.length)];
             const clickableElement = randomItem?.querySelector('.kng-list-new__cover');
             if (clickableElement && clickableElement.offsetWidth > 0 && clickableElement.offsetHeight > 0) {
                 clickableElement.click();
-                console.log('随机选择了一个视频');
+                console.log('随机选择未学习的视频');
             }
         }else {
-            // 如果没有找到目标，尝试翻页
-            const currentPageElement = document.querySelector('li.number.active'); // 获取当前页码元素
-            const currentPageNumber = parseInt(currentPageElement?.textContent.trim(), 10) || 1; // 获取当前页码值
-            const nextPage = document.querySelector(`li.number:nth-child(${currentPageNumber + 1})`); // 查找下一个页码按钮
-
+            // 未找到视频自动翻页
+            const currentPageElement = document.querySelector('li.number.active');
+            const currentPageNumber = parseInt(currentPageElement?.textContent.trim(), 10) || 1;
+            const nextPage = document.querySelector(`li.number:nth-child(${currentPageNumber + 1})`);
             if (nextPage) {
                 nextPage.click();
-                console.log('没有找到未看过的视频，正在翻页');
+                console.log('没有找到未学习的视频，自动翻页');
             } else {
                 console.log('已是最后一页，无法翻页');
             }
