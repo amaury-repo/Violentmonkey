69460850eb5b1e19935512324d24995666da3a0a
Amaury <amaury.data@gmail.com>
2025-01-07 20:51:39 +0800

    Update Siemens-YunXueTang-Assistant.user.js

--- Siemens-YunXueTang-Assistant.user.js
+++ Siemens-YunXueTang-Assistant.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Siemens YunXueTang Assistant
-// @namespace    https://github.com/Amaury-GitHub/Tampermonkey
+// @namespace    https://github.com/Amaury-GitHub/Violentmonkey
 // @version      1.1
 // @description  西门子云学堂自学习助手
 // @author       Amaury
