09f572af919c54092d8d27c6f9088506a05db823
Amaury <amaury.data@gmail.com>
2025-07-20 06:52:30 +0800

    Update TeslaMate_Charging_Prediction.user.js

--- TeslaMate_Charging_Prediction.user.js
+++ TeslaMate_Charging_Prediction.user.js
@@ -88,17 +88,6 @@
       return `${hours} h, ${mins} min`;
     }
 
-    // 格式化日期为 YYYY-MM-DD HH:MM:SS
-    function formatDateTime(date) {
-      const year = date.getFullYear();
-      const month = String(date.getMonth() + 1).padStart(2, '0');
-      const day = String(date.getDate()).padStart(2, '0');
-      const hours = String(date.getHours()).padStart(2, '0');
-      const minutes = String(date.getMinutes()).padStart(2, '0');
-      const seconds = String(date.getSeconds()).padStart(2, '0');
-      return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-    }
-
     // 页面显示结果
     predictionElement = document.createElement('div');
     predictionElement.style.position = 'fixed';
@@ -123,8 +112,8 @@
     // 内容区域
     const contentDiv = document.createElement('div');
     contentDiv.innerHTML = `
-      充电剩余时间&nbsp;&nbsp;&nbsp;${formatTime(estMinutes)}<br>
-      预计完成时间&nbsp;&nbsp;&nbsp;${finishTime.toLocaleTimeString()}
+      充电剩余时间&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${formatTime(estMinutes)}<br>
+      预计完成时间&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${finishTime.toLocaleTimeString()}
     `;
 
     // 组装
