d9191546c29db97370958c1c20b182c0c41241c3
Amaury <amaury.data@gmail.com>
2025-07-24 00:59:46 +0800

    Update Disable_Right-Click.user.js

--- Disable_Right-Click.user.js
+++ Disable_Right-Click.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/amaury-repo/Violentmonkey
 // @match       *://www.milkywayidle.com/*
 // @grant       none
-// @version     0.1
+// @version     20250724
 // @author      Amaury
 // @description 完全屏蔽右键
 // ==/UserScript==
@@ -11,6 +11,7 @@
 (function () {
     'use strict';
 
+    // 屏蔽右键菜单
     const disableContextMenu = (e) => {
         e.stopImmediatePropagation();
         e.preventDefault();
@@ -19,5 +20,15 @@
 
     window.addEventListener('contextmenu', disableContextMenu, true);
     document.addEventListener('contextmenu', disableContextMenu, true);
+
+    // 每小时自动刷新页面
+    const refreshInterval = 3600000;
+    
+    // 计时并刷新页面
+    setInterval(() => {
+        window.location.reload();
+    }, refreshInterval);
+
+    console.log('右键菜单已禁用，页面将每小时自动刷新一次');
 
 })();
