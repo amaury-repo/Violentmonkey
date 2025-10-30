dac81a4dfd2af1ad8dd68e03243d70f6a9143865
Amaury <amaury.data@gmail.com>
2025-10-30 21:47:09 +0800

    Update and rename Disable_Right-Click.user.js to Amaury-WMI.user.js

--- Amaury-WMI.user.js
+++ Amaury-WMI.user.js
@@ -1,34 +1,41 @@
 // ==UserScript==
-// @name        Disable Right-Click on MilkyWayIdle
+// @name        Optimizations for MilkyWayIdle
 // @namespace   https://github.com/amaury-repo/Violentmonkey
 // @match       *://www.milkywayidle.com/*
 // @grant       none
-// @version     20250724
+// @version     20251030
 // @author      Amaury
-// @description 完全屏蔽右键
+// @description 自用
 // ==/UserScript==
 
 (function () {
     'use strict';
 
+    // 修改字体
+    const fontName = "Sarasa Fixed SC";
+    const fontSize = "14px";
+    const style = document.createElement('style');
+    style.innerHTML = `
+        * {
+            font-family: '${fontName}', sans-serif !important;
+            font-size: ${fontSize} !important;
+        }
+    `;
+    document.head.appendChild(style);
+    
     // 屏蔽右键菜单
     const disableContextMenu = (e) => {
         e.stopImmediatePropagation();
         e.preventDefault();
         return false;
     };
-
     window.addEventListener('contextmenu', disableContextMenu, true);
     document.addEventListener('contextmenu', disableContextMenu, true);
 
     // 每小时自动刷新页面
     const refreshInterval = 3600000;
-    
-    // 计时并刷新页面
     setInterval(() => {
         window.location.reload();
     }, refreshInterval);
-
-    console.log('右键菜单已禁用，页面将每小时自动刷新一次');
 
 })();
