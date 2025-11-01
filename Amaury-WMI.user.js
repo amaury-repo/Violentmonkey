fd54ab122389a520c9b69acf6b796fb42f89b34f
Amaury <amaury.data@gmail.com>
2025-11-01 13:42:41 +0800

    Update Amaury-WMI.user.js

--- Amaury-WMI.user.js
+++ Amaury-WMI.user.js
@@ -72,7 +72,7 @@
     function formatNumber(value) {
         if (value >= 1_000_000_000) return (value / 1_000_000_000).toFixed(1).replace(/\.0$/, '') + 'B';
         if (value >= 1_000_000)     return (value / 1_000_000).toFixed(1).replace(/\.0$/, '') + 'M';
-        if (value >= 1_000)         return (value / 1_000).toFixed(1).replace(/\.0$/, '') + 'K';
+        if (value >= 1_000)         return (value / 1_000).toFixed(1).replace(/\.0$/, '') + 'k';
         return value.toFixed(0);
     }
     function extractOriginalCategories() {
